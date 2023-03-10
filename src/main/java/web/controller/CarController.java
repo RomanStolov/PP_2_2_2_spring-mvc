b49a35d89580b22928556f902a3783c416889b35
RomanStolov <roman_stolov@mail.ru>
2023-03-10 18:32:35 +0300

    1. Поправил замечание ментора относительно ненужности создания бина для класса Car указанием аннотации в том классе; 2. Добавил слой Service; 3. Вместо аннотаций @Component указал аннотации @Repository и @Service для CarDAOImpl и CarServiceImpl; 4. Обновил зависимости на самые последние актуальные версии в pom.xml.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
 import web.service.CarService;
 
 @Controller
